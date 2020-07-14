033657f100e3448d352e68effcd24ac53bf2a50d
fansichao <643566992@qq.com>
2020-07-15 01:36:06 +0800

    完善内容

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # auto_generate_markdown_url
 
-根据文件目录，自动生成 MarkDown URL 链接。
+功能说明
+
+- 根据文件目录，自动生成 MarkDown URL 链接。
+- 根据文件目录，自动生成 文件目录树
+- 适合 Windows 系统
+
+## 命令说明
+
+```bash
+PS D:\@vscode\auto_generate_markdown_url> python .\dirtree.py --help
+Usage: 根据目录 生成 文件目录树 和 MarkDown链接
+
+Options:
+  -h, --help            show this help message and exit
+  --name=FILE, --username=FILE
+                        Github 用户名称:
+  --repo=GITHUB_REPONAME, --reponame=GITHUB_REPONAME
+                        GitHub 仓库名称:
+  --local=LOCAL_PATH, --local_path=LOCAL_PATH
+                        本地文件目录:
+  --save=SAVE_PATH, --save_path=SAVE_PATH
+                        保存在Github的位置:
+  --tree=TREE_FILE_PATH, --tree_file_path=TREE_FILE_PATH
+                        目录树保存的文件名称:
+```
+
+## 命令样例
+
+```bash
+python .\dirtree.py --local_path=demo --save_path=/demo
+```
+
+## 目录树效果
 
 目录树，效果如下
 
@@ -20,17 +52,19 @@
 
 ```
 
-目录 MarkDown链接，效果如下
+## 目录 MarkDown 链接效果
 
-- [demo](https://github.com/fansichao/auto_generate_markdown_url/tree/master/)
-  - [A1](https://github.com/fansichao/auto_generate_markdown_url/tree/master/A1)
-    - [A11](https://github.com/fansichao/auto_generate_markdown_url/tree/master/A11)
-      - [demo110.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/A1/A11/demo110.txt)
-      - [readme.md](https://github.com/fansichao/auto_generate_markdown_url/blob/master/A1/A11/readme.md)
-    - [demo11.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/A1/demo11.txt)
-    - [demo12.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/A1/demo12.txt)
-    - [readme.md](https://github.com/fansichao/auto_generate_markdown_url/blob/master/A1/readme.md)
-  - [A2](https://github.com/fansichao/auto_generate_markdown_url/tree/master/A2)
-  - [demo01.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo01.txt)
-  - [demo02.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo02.txt)
-  - [readme.md](https://github.com/fansichao/auto_generate_markdown_url/blob/master/readme.md)
+目录 MarkDown 链接，效果如下
+
+- [demo](https://github.com/fansichao/auto_generate_markdown_url/tree/master/demo)
+  - [A1](https://github.com/fansichao/auto_generate_markdown_url/tree/master/demo/A1)
+    - [A11](https://github.com/fansichao/auto_generate_markdown_url/tree/master/demo/A11)
+      - [demo110.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/A1/A11/demo110.txt)
+      - [readme.md](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/A1/A11/readme.md)
+    - [demo11.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/A1/demo11.txt)
+    - [demo12.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/A1/demo12.txt)
+    - [readme.md](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/A1/readme.md)
+  - [A2](https://github.com/fansichao/auto_generate_markdown_url/tree/master/demo/A2)
+  - [demo01.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/demo01.txt)
+  - [demo02.txt](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/demo02.txt)
+  - [readme.md](https://github.com/fansichao/auto_generate_markdown_url/blob/master/demo/readme.md)
--- dirtree.py
+++ dirtree.py
@@ -1,11 +1,11 @@
 '''
 功能说明:
-- 根据文件目录 自动生成目录树
+- 根据文件目录 自动生成 文件目录树
 - 根据文件目录 自动生成 MarkDown URL 链接
 
 使用说明:
-
-
+- python .\dirtree.py --help
+- python .\dirtree.py --local_path=demo --save_path=/demo
 '''
 from pathlib import Path
 import copy
@@ -19,26 +19,24 @@ if not sys.version.startswith('3'):
 
 
 class DirectionTree(object):
-    """生成目录树
-    @ pathname: 目标目录
-    @ save_path: 要保存成文件的名称
-    """
 
-    def __init__(self,   local_path='.', save_path='tree.txt', github_username=None,
-                 github_reponame=None, ignore_items=None):
+    def __init__(self,   local_path='.', save_path='/', github_username=None,
+                 github_reponame=None, tree_file_path='tree.txt', ignore_items=None):
         u"""
             :params local_path: 本地文件路径
-            :params save_path: 保存文件路径. 目录树保存位置
+            :params save_path: 保存在Github的位置
             :params github_username: Github用户名称
             :params github_reponame: Github仓库名称
+            :params tree_file_path: 保存文件路径. 目录树保存位置           
             :params ignore_items: 忽略项
         """
         super(DirectionTree, self).__init__()
 
         self.pathname = Path(local_path)
-        self.save_path = save_path
+        self.tree_file_path = tree_file_path
         self.github_username = github_username
         self.github_reponame = github_reponame
+        self.save_path = save_path
 
         self.local_path_dirnames = self.get_base_dir(local_path)
         # 忽略项
@@ -60,9 +58,9 @@ class DirectionTree(object):
 
         exp:
             # github 有目录情况 文件实际路径
-            github_hav_dir_url = 'https://github.com/fansichao/python-knowledge-tree/tree/master/'
+            github_hav_dir_url = 'https://github.com/fansichao/auto_generate_markdown_url/tree/master/'
             # github 无目录情况 文件实际路径
-            github_not_dir_url = 'https://github.com/fansichao/python-knowledge-tree/blob/master/'
+            github_not_dir_url = 'https://github.com/fansichao/auto_generate_markdown_url/blob/master/'
         """
 
         self.github_hav_dir_url = '/'.join(['https://github.com', self.github_username,
@@ -74,9 +72,9 @@ class DirectionTree(object):
         " 重设本地目录 "
         self.pathname = Path(local_path)
 
-    def set_save_path(self, save_path):
+    def set_save_path(self, tree_file_path):
         " 重设保存文件 "
-        self.save_path = save_path
+        self.tree_file_path = tree_file_path
 
     def get_base_dir(self, filepath):
         '''
@@ -104,7 +102,7 @@ class DirectionTree(object):
         if self.pathname.is_file():
             dirnames = self.get_base_dir(self.pathname)
             # self.local_path_dirnames [WindowsPath('D:/'), '@vscode']
- 
+
             for i in self.local_path_dirnames:
                 # dirnames ['Code', 'dirtree.py']
                 if i in dirnames:
@@ -117,18 +115,28 @@ class DirectionTree(object):
             self.tree += '    |' * n + '-' * 4 + name + '\n'
             # path_name Code/dirtree.py
             path_name = '/'.join([str(a) for a in dirnames])
-            self.href += '    '*n + \
-                '- [%s](%s)' % (name, self.github_not_dir_url+'/'+path_name) + '\n'
 
+            if self.save_path == '/':
+                remote_github_path = self.github_not_dir_url + '/' + path_name
+            else:
+                remote_github_path = self.github_not_dir_url + self.save_path + '/' + path_name
+
+            self.href += '  '*n + \
+                '- [%s](%s)' % (name,  remote_github_path) + '\n'
         elif self.pathname.is_dir():
             name = str(self.pathname.relative_to(self.pathname.parent))
             self.tree += '    |' * n + '-' * 4 + name + '\\' + '\n'
-            if not bool(self.href):
-                self.href += '    '*n + \
-                    '- [%s](%s)' % (name, self.github_hav_dir_url+'/') + '\n'
+            if self.save_path == '/':
+                remote_github_path = self.github_hav_dir_url
             else:
-                self.href += '    '*n + \
-                    '- [%s](%s)' % (name, self.github_hav_dir_url+'/'+name) + '\n'
+                remote_github_path = self.github_hav_dir_url + self.save_path
+
+            if not bool(self.href):
+                self.href += '  '*n + \
+                    '- [%s](%s)' % (name, remote_github_path) + '\n'
+            else:
+                self.href += '  '*n + \
+                    '- [%s](%s)' % (name, remote_github_path+'/'+name) + '\n'
 
             for cp in self.pathname.iterdir():
                 if bool([i for i in self.ignore_items if str(cp).find(i) != -1]):
@@ -137,29 +145,32 @@ class DirectionTree(object):
                 self.generate_tree(n + 1)
 
     def save_file(self):
-        # with open(self.save_path, 'w',encoding='utf-8') as f:
-        with open(self.save_path, 'wb') as f:
+        # with open(self.tree_file_path, 'w',encoding='utf-8') as f:
+        with open(self.tree_file_path, 'wb') as f:
             f.write(self.tree.encode('utf-8'))
 
 
 if __name__ == '__main__':
     # 配置默认参数
-    defalut_save_path = 'tree.txt'
+    defalut_tree_file_path = 'tree.txt'
     defalut_local_path = 'D:\\@vscode\\auto_generate_markdown_url'
     defalut_local_path = '.'
     default_github_username = 'fansichao'
     default_github_reponame = 'auto_generate_markdown_url'
+    defalut_save_path = '/'
 
-    usage = "根据目录 生成 目录树 和 MarkDown链接"
+    usage = "根据目录 生成 文件目录树 和 MarkDown链接"
     parser = OptionParser(usage)  # 带参的话会把参数变量的内容作为帮助信息输出
     parser.add_option("--name", "--username", dest="github_username", action="store",
-                      help="输入 Github 用户名称:", metavar="FILE", type="string", default=default_github_username)
+                      help="Github 用户名称:", metavar="FILE", type="string", default=default_github_username)
     parser.add_option("--repo", "--reponame", dest="github_reponame", action="store",
-                      help="输入 GitHub 仓库名称:", default=default_github_reponame)
-    parser.add_option("--l", "--local_path", dest="local_path", action="store",
-                      help="输入本地文件目录:", default=defalut_local_path)
-    parser.add_option("--s", "--save_path", dest="save_path", action="store",
-                      help="输入保存的文件名称:", default=defalut_save_path)
+                      help="GitHub 仓库名称:", default=default_github_reponame)
+    parser.add_option("--local", "--local_path", dest="local_path", action="store",
+                      help="本地文件目录:", default=defalut_local_path)
+    parser.add_option("--save", "--save_path", dest="save_path", action="store",
+                      help="保存在Github的位置:", default=defalut_save_path)
+    parser.add_option("--tree", "--tree_file_path", dest="tree_file_path", action="store",
+                      help="目录树保存的文件名称:", default=defalut_tree_file_path)
 
     (options, args) = parser.parse_args()
     print('> 输入参数如下: \n', options)
